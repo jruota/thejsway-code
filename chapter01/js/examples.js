d3bbffb047ebc9c6ee4bc8843b75dc67d6258c27
Jarosław Kołosowski <jarokolosowski@gmail.com>
2022-05-22 10:53:38 +0200

    Chapter 1

--- chapter01/js/examples.js
+++ chapter01/js/examples.js
@@ -1,1 +1,13 @@
 // JavaScript code goes here
+console.log("Hello from JavaScript!");
+// console.log("Let's do some math");
+console.log(4 + 7);
+// console.log(12 / 0);
+console.log("Goodbye!");
+
+console.log("Jarosław");
+console.log(34);
+
+console.log(4 + 5); // 9
+console.log("4 + 5"); // "4 + 5"
+console.log("4" + "5"); // "45")
