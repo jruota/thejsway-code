593ae20527978d8656c6b39712376aefbf823ba9
Jarosław Kołosowski <jarokolosowski@gmail.com>
2022-06-03 08:46:35 +0200

    Chapter 8

--- chapter08/js/examples.js
+++ chapter08/js/examples.js
@@ -1,1 +1,60 @@
 // JavaScript code goes here
+
+// Word Info
+
+const word = prompt("Type a word:");
+console.log(word.length, word.toUpperCase(), word.toLowerCase());
+
+// ############################################################################
+
+// Vowel Count
+
+let vowelCounter = 0;
+for (const letter of word) {
+  if (
+    letter === "a" ||
+    letter === "e" ||
+    letter === "i" ||
+    letter === "o" ||
+    letter === "u"
+  ) {
+    vowelCounter++;
+  }
+}
+
+console.log(`${word} has ${vowelCounter} vowels.`);
+
+// ############################################################################
+
+// Backwards Word
+
+function backwardsWord(word) {
+  const letters = Array.from(word);
+
+  let bW = "";
+  letters.forEach((letter) => {
+    bW = letter + bW;
+  });
+
+  return bW;
+}
+
+console.log(`${word} backwards is ${backwardsWord(word)}.`);
+
+// ############################################################################
+
+// Palindrome
+
+function isPalindrome(word) {
+  const lowerCaseWord = word.toLowerCase();
+  if (lowerCaseWord === backwardsWord(lowerCaseWord)) {
+    return true;
+  }
+  return false;
+}
+
+if (isPalindrome(word)) {
+  console.log(`${word} is a palindrome.`);
+} else {
+  console.log(`${word} is not a palindrome.`);
+}
