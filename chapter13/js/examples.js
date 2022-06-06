6d6ff5432533168381cdca5badc243d7f533dcdf
Jarosław Kołosowski <jarokolosowski@gmail.com>
2022-06-06 09:59:18 +0200

    Chapter 13

--- chapter13/js/examples.js
+++ chapter13/js/examples.js
@@ -1,1 +1,23 @@
 // JavaScript code goes here
+
+function showChild(node, index) {
+  if (node.nodeType == document.TEXT_NODE) {
+    console.error("Wrong node type");
+  } else if (index >= node.childNodes.length || index < 0) {
+    console.error("Incorrect index");
+  } else {
+    console.log(node.childNodes[index]);
+  }
+}
+
+// Should show the h1 node
+showChild(document.body, 1);
+
+// Should show "Incorrect index"
+showChild(document.body, -1);
+
+// Should show "Incorrect index"
+showChild(document.body, 8);
+
+// Should show "Wrong node type"
+showChild(document.body.childNodes[0], 0);
